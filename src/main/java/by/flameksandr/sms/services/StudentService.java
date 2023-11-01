bf739ed08e617f197abc96d73b06245f00b43351
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-11-01 09:46:21 +0300

    added interface StudentService, and StudentServiceImpl

---
+++ src/main/java/by/flameksandr/sms/services/StudentService.java
@@ -0,0 +1,4 @@
+package by.flameksandr.sms.services;
+
+public interface StudentService {
+}
---
+++ src/main/java/by/flameksandr/sms/services/impl/StudentServiceImpl.java
@@ -0,0 +1,6 @@
+package by.flameksandr.sms.services.impl;
+
+import by.flameksandr.sms.services.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+}
